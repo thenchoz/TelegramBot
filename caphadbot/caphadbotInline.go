b318d8c2206467aa34cb25b680d72de9b9818cdf
thenchoz <tristan.henchoz@gmail.com>
2024-12-14 22:14:23 +0100

    correct bug

--- caphadbot/caphadbotInline.go
+++ caphadbot/caphadbotInline.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"strconv"
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -28,70 +27,67 @@ func handleInline(
 		Results:       []interface{}{},
 	}
 
-	for i := range 2 {
+	var msg, title, description, url string
+	switch {
+	case strings.HasPrefix("insult", inline.Query):
+		title = "Insult"
+		msg, _, _, err = handleCommand(ctx, "insult", user, bot)
+		description = "Random insult"
 
-		var msg, title, description, url string
-		switch {
-		case strings.HasPrefix("insult", inline.Query):
-			title = "Insult"
-			msg, _, _, err = handleCommand(ctx, "insult", user, bot)
-			description = "Random insult"
+	case strings.HasPrefix("joke", inline.Query):
+		title = "Joke"
+		msg, _, _, err = handleCommand(ctx, "joke", user, bot)
+		description = "Random joke"
 
-		case strings.HasPrefix("joke", inline.Query):
-			title = "Joke"
-			msg, _, _, err = handleCommand(ctx, "joke", user, bot)
-			description = "Random joke"
+	case strings.HasPrefix("hpquote", inline.Query):
+		title = "Quote"
+		msg, url, _, err = handleCommand(ctx, "hpquote", user, bot)
+		description = "Random quote from Harry Potter"
 
-		case strings.HasPrefix("hpquote", inline.Query):
-			title = "Quote"
-			msg, url, _, err = handleCommand(ctx, "hpquote", user, bot)
-			description = "Random quote from Harry Potter"
+	case strings.HasPrefix("quote", inline.Query):
+		title = "Citation"
+		msg, url, _, err = handleCommand(ctx, "quote", user, bot)
+		description = "Random quote"
 
-		case strings.HasPrefix("quote", inline.Query):
-			title = "Citation"
-			msg, url, _, err = handleCommand(ctx, "quote", user, bot)
-			description = "Random quote"
+	case strings.HasPrefix("spell", inline.Query):
+		title = "Spell"
+		msg, _, _, err = handleCommand(ctx, "spell", user, bot)
+		description = "Random spell"
 
-		case strings.HasPrefix("spell", inline.Query):
-			title = "Spell"
-			msg, _, _, err = handleCommand(ctx, "spell", user, bot)
-			description = "Random spell"
+	case strings.HasPrefix("spell_explained", inline.Query):
+		title = "Spell"
+		msg, _, _, err = handleCommand(ctx, "spell_explained", user, bot)
+		description = "Random spell explained"
 
-		case strings.HasPrefix("spell_explained", inline.Query):
-			title = "Spell"
-			msg, _, _, err = handleCommand(ctx, "spell_explained", user, bot)
-			description = "Random spell explained"
+	case strings.HasPrefix("help", inline.Query):
+		title = "Helper"
+		msg, _, _, err = handleCommand(ctx, "help", user, bot)
+		description = "Helper text"
 
-		case strings.HasPrefix("help", inline.Query):
-			title = "Helper"
-			msg, _, _, err = handleCommand(ctx, "help", user, bot)
-			description = "Helper text"
+	default:
+		msg = "unknown"
+		title = "Unknown"
+		description = "Unknown command"
+	}
 
-		default:
-			msg = "unknown"
-			title = "Unknown"
-			description = "Unknown command"
-		}
+	if err != nil {
+		return
+	}
 
-		if err != nil {
-			return
-		}
+	id := inline.ID
+	article := tgbotapi.NewInlineQueryResultArticleMarkdownV2(id, title, msg)
+	article.Description = description
 
-		id := inline.ID + strconv.Itoa(i)
-		article := tgbotapi.NewInlineQueryResultArticleMarkdownV2(id, title, msg)
-		article.Description = description
+	inlineConf.Results = append(inlineConf.Results, article)
 
-		inlineConf.Results = append(inlineConf.Results, article)
+	if url != "" {
+		photo := tgbotapi.NewInlineQueryResultPhotoWithThumb(id+"pic", url, url)
+		photo.Caption = msg
+		photo.Description = description + " with picture"
+		photo.Title = title
+		photo.ParseMode = "markdown"
 
-		if url != "" {
-			photo := tgbotapi.NewInlineQueryResultPhotoWithThumb(id+"pic", url, url)
-			photo.Caption = msg
-			photo.Description = description + " with picture"
-			photo.Title = title
-			photo.ParseMode = "markdown"
-
-			inlineConf.Results = append(inlineConf.Results, photo)
-		}
+		inlineConf.Results = append(inlineConf.Results, photo)
 	}
 
 	_, err = bot.tgBot.Request(inlineConf)
